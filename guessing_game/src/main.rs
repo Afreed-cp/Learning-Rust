df1d87d65a1c45e5e37718026196111de50663d1
Afreed <afreed.cp@phases.io>
2024-11-24 11:47:34 +0530

    looped

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,25 +1,31 @@
 use std::{cmp::Ordering, io, u32};
 
-use rand::{Rng};
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     println!("Input your guess");
 
-    let secret_num:u32  = rand::thread_rng().gen_range(1..=10);
+    let secret_num: u32 = rand::thread_rng().gen_range(1..=10);
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
+        let parsed_guess: u32 = guess
+            .trim()
+            .parse::<u32>()
+            .expect("Failed to parse your guess");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let parsed_guess : u32 = guess.trim().parse::<u32>().expect("Failed to parse your guess");
-
-    match parsed_guess.cmp(&secret_num) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win"),
+        match parsed_guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
